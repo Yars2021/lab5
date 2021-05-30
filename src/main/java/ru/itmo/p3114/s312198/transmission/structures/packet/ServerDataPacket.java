8013624106c88e50f61d8c81eace17bc675e88e3
Yars2021 <yaroslav.sukhovey@yandex.ru>
2021-05-30 15:51:13 +0300

    Added ServerDataPacket

--- src/main/java/ru/itmo/p3114/s312198/transmission/structures/packet/ServerDataPacket.java
+++ src/main/java/ru/itmo/p3114/s312198/transmission/structures/packet/ServerDataPacket.java
@@ -1,6 +1,29 @@
 package ru.itmo.p3114.s312198.transmission.structures.packet;
 
+import ru.itmo.p3114.s312198.command.CommandOutput;
+
 import java.io.Serializable;
 
 public class ServerDataPacket implements Serializable {
+    private final String serverMessage;
+    private final CommandOutput commandOutput;
+    private final boolean executed;
+
+    public ServerDataPacket(String serverMessage, CommandOutput commandOutput, boolean executed) {
+        this.serverMessage = serverMessage;
+        this.commandOutput = commandOutput;
+        this.executed = executed;
+    }
+
+    public String getServerMessage() {
+        return serverMessage;
+    }
+
+    public CommandOutput getCommandOutput() {
+        return commandOutput;
+    }
+
+    public boolean isExecuted() {
+        return executed;
+    }
 }
